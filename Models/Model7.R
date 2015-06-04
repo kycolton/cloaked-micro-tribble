4ecb2d84893b686e2f3b77e000d5dcdc97bf40d3
Kyle <ky.colton@gmail.com>
2015-06-04 10:14:45 -0700

    Random Forest
    17.5 hour runtime, decent performance, but not great.

---
+++ Models/Model7.R
@@ -0,0 +1,47 @@
+# Library Required
+library(randomForest)
+
+# Read the training data
+train <- read.csv('Data/train.csv',header=T)
+test <- read.csv('Data/test.csv',header=T)
+
+# variables to be used (taken from mod4)
+vars <- c(4,9,99,8,13:32,34:40,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72:85,87,89,91,92,95:97)
+
+# Recode Deed_type in training data
+train$deed_simple <- as.character(train$deed_type)
+train$deed_simple[-which(train$deed_type %in% c('WD','SJ','SW','RD'))] <- 'Other'
+train$deed_simple <- as.factor(train$deed_simple)
+
+# Recode Deed_type in test data
+test$deed_simple <- as.character(test$deed_type)
+test$deed_simple[-which(test$deed_type %in% c('WD','SJ','SW','RD'))] <- 'Other'
+test$deed_simple <- as.factor(test$deed_simple)
+
+# Set seed
+set.seed(53924)
+
+# Model
+tic <- proc.time()
+deforestation <- randomForest(totalActualVal~.,data=train[,vars],importance=T,ntree=200,nodesize=5)
+tock <- proc.time()
+tock - tic
+
+for(i in vars)
+{
+  if(is.factor(test[,i]))
+  {
+    print(names(test)[i])
+    levels(test[,i]) <- levels(train[,i])
+  }
+}
+
+# Need to reset levels that aren't represented in test, otherwise the model gets angry
+#levels(test$ConstCodeDscr) <- levels(train$ConstCodeDscr)
+#(test$qualityCodeDscr) <- levels(train$qualityCodeDscr)
+
+# Predict
+mod7 <- predict(deforestation,test[,vars])
+mod7 <- as.data.frame(cbind(1:10000,mod7))
+names(mod7) <- c('id','totalActualVal')
+write.csv(mod7,file='mod7.csv',row.names=F)
